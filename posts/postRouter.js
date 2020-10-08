c8c0f49d4991f442aa24b81c0c992e3050c5c004
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-10-08 16:32:17 -0500

    mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -37,22 +37,20 @@ router.get('/:id', (req, res) => {
 router.post("/", (req, res) => {
     const { title, contents } = req.body;
     if (!title || !contents) {
-      res
-        .status(400)
-        .json({
-          errorMessage: "Please provide title and contents for the post."
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
         });
     }
     posts
-      .insert(req.body)
-      .then(post => {
-        res.status(201).json(post);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          message: "There was an error while saving the post to the database"
-        });
+        .insert(req.body)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            });
       });
 });
   
@@ -67,18 +65,18 @@ router.post("/:id/comments", (req, res) => {
     posts
         .findPostComments(id)
         .then(post => {
-        if (!post) {
-            res
-            .status(404)
-            .json({ message: "The post with the specified ID does not exist." });
-        }
-        res.status(201).json(text);
+            if (!post) {
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist." 
+                });
+            }
+            res.status(201).json(text);
         })
         .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            message: "There was an error while saving the comment to the database."
-        });
+            console.log(err);
+            res.status(500).json({
+                message: "There was an error while saving the comment to the database."
+            });
         });
 });
 
@@ -86,17 +84,17 @@ router.get("/:id/comments", (req, res) => {
     posts
         .findPostComments(req.params.id)
         .then(comment => {
-        if (comment) {
-            res.status(200).json(comment);
-        } else {
-            res.status(404).json({ message: "comment not found." });
-        }
+            if (comment) {
+                res.status(200).json(comment);
+            } else {
+                res.status(404).json({ message: "comment not found." });
+            }
         })
         .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            message: "Error retrieving the comments."
-        });
+            console.log(err);
+            res.status(500).json({
+                message: "Error retrieving the comments."
+            });
         });
 });
 
@@ -111,30 +109,29 @@ router.delete("/:id", (req, res) => {
     });
     posts.remove(req.params.id).catch(err => {
         console.log(err);
-        res
-        .status(500)
-        .json({ errorMessage: "The posts information could not be retrieved." });
+        res.status(500).json({ 
+            errorMessage: "The posts information could not be retrieved."
+        });
     });
 });
 
 router.put("/:id", (req, res) => {
-    const { title, contents } = req.body;
     const changes = req.body;
     const { id } = req.params;
     posts
         .update(id, changes)
         .then(post => {
-        if (!post) {
-            res.status(404).json({ message: "The post could not be found." });
-        } else {
-            res.status(200).json(post);
-        }
+            if (!post) {
+                res.status(404).json({ message: "The post could not be found." });
+            } else {
+                res.status(200).json(post);
+            }
         })
         .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            message: "Error updating the post"
-        });
+            console.log(err);
+            res.status(500).json({
+                message: "Error updating the post"
+            });
         });
 });
 
